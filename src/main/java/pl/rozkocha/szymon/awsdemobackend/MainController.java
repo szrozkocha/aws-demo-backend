148f277814bf747d41eaaccd31f6065b89bdf7a0
Szymon Rozkocha <rozkocha@wszib.edu.pl>
2022-02-28 16:05:17 +0100

    move terraform to infrastructure repo

--- src/main/java/pl/rozkocha/szymon/awsdemobackend/MainController.java
+++ src/main/java/pl/rozkocha/szymon/awsdemobackend/MainController.java
@@ -1,12 +1,18 @@
 package pl.rozkocha.szymon.awsdemobackend;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-    @GetMapping(value = "/api/status")
+    @GetMapping("/")
+    public ResponseEntity<Void> health() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/status")
     public StatusResponse status() {
         return new StatusResponse("OK");
     }
