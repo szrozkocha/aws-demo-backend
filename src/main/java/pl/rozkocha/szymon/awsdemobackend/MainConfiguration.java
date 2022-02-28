1edde93e742df19b990e63916201b357e0b8fe81
Szymon Rozkocha <rozkocha@wszib.edu.pl>
2022-02-28 19:42:55 +0100

    updated communication with frontend

---
+++ src/main/java/pl/rozkocha/szymon/awsdemobackend/MainConfiguration.java
@@ -0,0 +1,22 @@
+package pl.rozkocha.szymon.awsdemobackend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class MainConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*")
+                        .allowedOrigins("http://localhost:4200", "https://rozkocha.com", "https://www.rozkocha.com");
+            }
+        };
+    }
+}
--- src/main/java/pl/rozkocha/szymon/awsdemobackend/MainController.java
+++ src/main/java/pl/rozkocha/szymon/awsdemobackend/MainController.java
@@ -1,6 +1,5 @@
 package pl.rozkocha.szymon.awsdemobackend;
 
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
